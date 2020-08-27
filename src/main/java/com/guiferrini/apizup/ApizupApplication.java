8177609842f9dda0c2ec73d4091537a0c07ec8d7
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-27 17:19:42 -0300

    Projeto criado

---
+++ src/main/java/com/guiferrini/apizup/ApizupApplication.java
@@ -0,0 +1,13 @@
+package com.guiferrini.apizup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApizupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApizupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guiferrini/apizup/ApizupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiferrini.apizup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApizupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
